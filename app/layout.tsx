3da44ba82e0fed833e9c7b78ea36ee984d62ab6a
Matt Dean <matt.thomas.dean@gmail.com>
2025-11-30 20:30:58 -0500

    analytics vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
 
 export const metadata: Metadata = {
   title: "Premier League API",
@@ -15,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Analytics />
       </body>
     </html>
   );
